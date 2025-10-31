6339a663513be89e015abb396cb683093499c04b
MaxIliv <max.r.iliv@gmail.com>
2025-10-31 20:16:28 +0200

    res.write, res.end

--- 6-Node.js/wild-horizons/server.js
+++ 6-Node.js/wild-horizons/server.js
@@ -3,7 +3,14 @@ import http from 'node:http';
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from server!') // sends data
+  res.write('content');
+  res.write('content 2');
+
+  // must finish
+  res.end();
+  // res.end('Hello from server!', 'utf-8', () => {
+  //   console.log('response end!')
+  // }) // sends data
 })
 
 server.listen(PORT, () => {
