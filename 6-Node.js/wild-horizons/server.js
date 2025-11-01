2173a0676123dbca155e031c46ba7c1c4fd08776
MaxIliv <max.r.iliv@gmail.com>
2025-11-01 19:33:18 +0200

    continent filtering

--- 6-Node.js/wild-horizons/server.js
+++ 6-Node.js/wild-horizons/server.js
@@ -3,30 +3,33 @@ import { getDataFromDB } from './database/db.js';
 
 const PORT = 8000;
 
-
 const server = http.createServer(async (req, res) => {
   const destinations = await getDataFromDB();
 
-  if (
-    req.url === '/api'
-    && req.method === 'GET'
-  ) {
+  if (req.url === '/api' && req.method === 'GET') {
     res.setHeader('Content-Type', 'application/json');
     res.statusCode = 200;
-    res.end(
-      JSON.stringify(destinations)
-    );
+    res.end(JSON.stringify(destinations));
+  } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
+    const continent = req.url.split('/').pop();
+
+    const filteredData = destinations.filter(d => d.continent.toLowerCase() === continent.toLowerCase());
+
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    res.end(JSON.stringify(filteredData));
   } else {
     res.setHeader('Content-Type', 'application/json');
     res.statusCode = 404;
     res.end(
-      JSON.stringify({ error: 'Not found', message: 'The requested route does not exist' })
-    )
+      JSON.stringify({
+        error: 'Not found',
+        message: 'The requested route does not exist',
+      })
+    );
   }
-
-
-})
+});
 
 server.listen(PORT, () => {
   console.log('Server running on port:', PORT);
-})
+});
