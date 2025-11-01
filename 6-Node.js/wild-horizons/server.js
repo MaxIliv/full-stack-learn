407b26de145e3a3ef0e9e7a6309e741430589725
MaxIliv <max.r.iliv@gmail.com>
2025-11-01 19:29:52 +0200

    api data

--- 6-Node.js/wild-horizons/server.js
+++ 6-Node.js/wild-horizons/server.js
@@ -1,14 +1,30 @@
 import http from 'node:http';
+import { getDataFromDB } from './database/db.js';
 
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {
+
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB();
+
   if (
     req.url === '/api'
     && req.method === 'GET'
   ) {
-    res.end('Yes!');
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    res.end(
+      JSON.stringify(destinations)
+    );
+  } else {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 404;
+    res.end(
+      JSON.stringify({ error: 'Not found', message: 'The requested route does not exist' })
+    )
   }
+
+
 })
 
 server.listen(PORT, () => {
