a961478d6c5318890e167e76dde3461b59f9d976
MaxIliv <max.r.iliv@gmail.com>
2025-11-03 19:52:11 +0200

    writeHead

--- 6-Node.js/from-the-other-side/server.js
+++ 6-Node.js/from-the-other-side/server.js
@@ -3,9 +3,20 @@ import http from 'node:http'
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html; charset=utf-8')
-  res.end('<html><h1>The server is working</h1></html>')
+
+  // included
+  res.setHeader('Content-Type', 'text/html');
+
+  // sends headers immideately
+  // overrides Content type
+  res.writeHead(200, 'OK', {
+    'Content-Type': 'text/html'
+  });
+
+  // Ignored
+  res.setHeader('Content-Type', 'text/html');
+
+  res.end('<html><h1>The server is working</h1></html>');
 })
 
 server.listen(PORT, () => {
