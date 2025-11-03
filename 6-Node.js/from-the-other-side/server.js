15c79b0b17aed4773c8a4ebd51ff5382d9c7894f
MaxIliv <max.r.iliv@gmail.com>
2025-11-03 20:20:29 +0200

    static serve fn

--- 6-Node.js/from-the-other-side/server.js
+++ 6-Node.js/from-the-other-side/server.js
@@ -1,25 +1,35 @@
 import http from 'node:http'
+import path from 'node:path';
+
+import { serveStatic } from './utils/serveStatic.js';
+// 1. [x] Identify what resources client want?
+// 2. [x] Identify path to that resource
+//    [x] The current modules directory
+//    [x] Path to the resource from that directory
+// 3. [] Read resources with FS
+// 4. [] Send to client
 
 const PORT = 8000;
 
 // console.log(import.meta);
 // __dirname - old school node.js
 // console.log(import.meta.dirname);
-
-const __dirname = import.meta.dirname;
-console.group('dirname', __dirname);
-
+// console.group('dirname', __dirname);
 // Path module
 // Current Working Directry CWD
-console.group('CWD', process.cwd())
+// console.group('CWD', process.cwd())
+
+// Absolute path 
+
+// Relative path
+
+const __dirname = import.meta.dirname;
+
+serveStatic(__dirname);
 
 const server = http.createServer((req, res) => {
-  // 1. [x] Identify what resources client want?
-  // 2. [] Identify path to that resource
-  //    [] The current modules directory
-  //    [] Path to the resource from that directory
-  // 3. [] Read resources with FS
-  // 4. [] Send to client
+
+  const asbolutePathToResource = path.join(__dirname, 'public', 'index.html');
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
---
+++ 6-Node.js/from-the-other-side/utils/serveStatic.js
@@ -0,0 +1,8 @@
+import path from 'node:path';
+
+export function serveStatic(baseDir) {
+
+  const filePath = path.join(baseDir, 'public', 'index.html');
+
+  console.log(filePath);
+}
