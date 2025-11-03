3d0132e48e5f7b112cdd1be3dbd972d5d53fd128
MaxIliv <max.r.iliv@gmail.com>
2025-11-03 19:32:31 +0200

    CORS settings

--- 6-Node.js/wild-horizons/utils/sendJSONResponse.js
+++ 6-Node.js/wild-horizons/utils/sendJSONResponse.js
@@ -1,5 +1,12 @@
 export const sendJSONResponse = (res, statusCode, payload) => {
   res.setHeader('Content-Type', 'application/json');
+
+  // CORS
+  // Allow any domain/protocol/port
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Only for specific methods
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
   res.statusCode = statusCode;
   res.end(JSON.stringify(payload));
 }
