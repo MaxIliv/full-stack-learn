78ea4bac4c7f92eb69b4f85d8b9c75c4e0339c4b
MaxIliv <max.r.iliv@gmail.com>
2025-11-03 19:58:44 +0200

    define steps

--- 6-Node.js/from-the-other-side/server.js
+++ 6-Node.js/from-the-other-side/server.js
@@ -3,19 +3,15 @@ import http from 'node:http'
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
+  // 1. [] Identify what resources client want?
+  // 2. [] Identify path to that resource
+  //    [] The current modules directory
+  //    [] Path to the resource from that directory
+  // 3. [] Read resources with FS
+  // 4. [] Send to client
 
-  // included
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-
-  // sends headers immideately
-  // overrides Content type
-  res.writeHead(200, 'OK', {
-    'Content-Type': 'text/html'
-  });
-
-  // Ignored
-  res.setHeader('Content-Type', 'text/html');
-
   res.end('<html><h1>The server is working</h1></html>');
 })
 
