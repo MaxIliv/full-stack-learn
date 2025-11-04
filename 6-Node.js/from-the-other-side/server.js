240afd323bfaca32b1a4325f70e3e62644d31d0d
MaxIliv <max.r.iliv@gmail.com>
2025-11-04 16:49:39 +0200

    create getData

--- 6-Node.js/from-the-other-side/server.js
+++ 6-Node.js/from-the-other-side/server.js
@@ -1,11 +1,5 @@
 import http from 'node:http';
 import { serveStatic } from './utils/serveStatic.js';
-// 1. [x] Identify what resources client want?
-// 2. [x] Identify path to that resource
-//    [x] The current modules directory
-//    [x] Path to the resource from that directory
-// 3. [x] Read resources with FS
-// 4. [] Send to client
 
 const PORT = 8000;
 const __dirname = import.meta.dirname;
